16a90abaea3e974b121ca1b649a5abc974f1e6ed
kian <kian@chai.finance>
2021-07-17 17:49:23 +0900

    파일 업로드 추가

---
+++ src/main/java/com/bside/app/controller/web/FileUploadController.java
@@ -0,0 +1,23 @@
+package com.bside.app.controller.web;
+
+import com.bside.app.util.S3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class FileUploadController {
+
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+        String s3Url = s3Uploader.upload(multipartFile, "static");
+        return s3Url;
+    }
+}
