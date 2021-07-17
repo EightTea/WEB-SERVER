8237da9e10a70e32c04099df0025c24177fe017a
kian <kian@chai.finance>
2021-07-17 17:49:59 +0900

    spring 실행 환경파일 분리 ( 기본 + AWS S3 계정 )

--- src/main/java/com/bside/app/AppApplication.java
+++ src/main/java/com/bside/app/AppApplication.java
@@ -2,9 +2,14 @@ package com.bside.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class AppApplication {
+
+	static {
+		System.setProperty("spring.config.location", "classpath:/application.yml,classpath:/aws.yml");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
